901264901cacb923bd9b6f424f7cf859f7c9229c
dioliviers <dioliviers@gmail.com>
2023-03-07 00:59:31 -0300

    pega as imagens inseridas no widgeth criado pro header e faz elas ficarem rondomicas conforme o valor pré definido na function para tratar as imagens

--- app/public/wp-content/themes/wp-devs/general-template.php
+++ app/public/wp-content/themes/wp-devs/general-template.php
@@ -6,6 +6,11 @@ Template Name: General Template
 /* criei um modelo padrão de template que pode ser replicado em qualquer pagina que chamar o template (general) */
 ?>
 <?php get_header(); ?>
+
+<img src="<?php  header_image();?>" height="<?php echo get_custom_header()-> height; ?>" 
+width="<?php echo get_custom_header() -> width; ?>" alt="" /> <!-- após adicionar as imagens em aparencia -> header, adicionamos
+As imagens que irão ficar randomicas e logo após com essa chamada acima ele pega o header_image , coloca o height definido na function
+ e exibe com o echo o tamanho pre definido e a larguda pre definida com o width -->
         <div id="content" class="site-content">
             <div id="primary" class="content-area">
                 <main id="main" class="site-main">
--- app/public/wp-content/themes/wp-devs/page.php
+++ app/public/wp-content/themes/wp-devs/page.php
@@ -1,4 +1,9 @@
 <?php get_header(); ?>
+<img src="<?php  header_image();?>" height="<?php echo get_custom_header()-> height; ?>" 
+width="<?php echo get_custom_header() -> width; ?>" alt="" /> <!-- após adicionar as imagens em aparencia -> header, adicionamos
+As imagens que irão ficar randomicas e logo após com essa chamada acima ele pega o header_image , coloca o height definido na function
+ e exibe com o echo o tamanho pre definido e a larguda pre definida com o width -->
+ 
         <div id="content" class="site-content">
             <div id="primary" class="content-area">
                 <main id="main" class="site-main">
