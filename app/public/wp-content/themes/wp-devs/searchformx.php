7abaa422a38ace2b6493e1f4e3773f0a5f02e9fe
dioliviers <dioliviers@gmail.com>
2023-03-15 21:50:44 -0300

    cria pagina para busca, para não usar a padrão do wp pois se houver alguma atualização na pagina padrão vamos perder as informações

---
+++ app/public/wp-content/themes/wp-devs/searchformx.php
@@ -0,0 +1,13 @@
+<?php 
+    
+?>
+
+<form role="search" method="get" id="searchform" class="searchform" action=" <?php echo esc_url(home_url( '/' ) ) ?>"><!-- faz a esc_url
+fazer as buscas através da home -->
+    <div>
+        <label class="screen-reader-text" for="s">Search for:</label>
+        <input type="text" value="<?php echo get_search_query(); ?>" name="s" id="s"> <!-- get_search_query faz uma query das informações
+    disponiveis no site -->
+        <input type="submit" id="searchsubmit" value="Search">
+    </div>
+</form>
