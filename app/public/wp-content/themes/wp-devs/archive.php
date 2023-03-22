fc5e6a217783feaf27d21ef9432cc8952525ecfb
dioliviers <dioliviers@gmail.com>
2023-03-21 23:41:47 -0300

    criando uma archive.php

---
+++ app/public/wp-content/themes/wp-devs/archive.php
@@ -0,0 +1,48 @@
+
+<!-- só pelo nome archive.php o wordpress já identifica que é uma archive OBS:uma archive ela exibe todas as singles e etc  -->
+<?php get_header(); ?>
+
+<img src="<?php header_image(); ?>" height="<?php echo get_custom_header()->height; ?>" width="<?php echo get_custom_header()->width; ?>" alt="" />
+
+        <div id="content" class="site-content">
+            <div id="primary" class="content-area">
+                <main id="main" class="site-main">
+                    <div class="container">
+                        <?php the_archive_title('<h1 class= "archive-title">' , ' </h1> '); ?> <!-- Define o titulo da archive -->
+                        <?php the_archive_description('<div class="archive-description">' , ' </div>'); ?>
+                        <div class="archive-items">
+                            <?php 
+                                if( have_posts() ):
+                                    while( have_posts() ) : the_post();
+                                    ?>
+                                        <article>
+                                            <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+                                            <div class="meta-info">
+                                                <p>Posted in <?php echo get_the_date(); ?> by <?php the_author_posts_link(); ?></p> <!--  pega a data e o autor do post -->
+                                                <p>Categories: <?php the_category( ' ' ); ?></p>
+                                                <p>Tags: <?php the_tags( '', ', '); ?></p>
+                                            </div>
+                                            <?php the_excerpt(); ?>
+                                        </article>
+                                    <?php
+                                    endwhile;
+                                    ?>
+                                        <div class="wpdevs-pagination">
+                                            <div class="pages new">
+                                                <?php previous_posts_link( '<< Newer posts' ); ?>  <!--  essa função cria uma paginação para next -->
+                                            </div>
+                                            <div class="pages old">
+                                                <?php next_posts_link( 'Older posts >>' ); ?> <!--  essa função cria uma paginação para back -->
+                                            </div>
+                                        </div>
+                                    <?php
+                                else: ?>
+                                    <p>Nothing yet to be displayed!</p>
+                            <?php endif; ?>                                
+                        </div>
+                        <?php get_sidebar(); ?>
+                    </div>
+                </main>
+            </div>
+        </div>
+<?php get_footer(); ?>
